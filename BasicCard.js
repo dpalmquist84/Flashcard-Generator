5f764dfe1cf3c4970d2e7ca8fe07959a86dfaf91
David Palmquist <dpalmquist84@gmail.com>
2017-09-01 10:49:23 -0400

    added fullText and partial variable.  Made logic for the guesses

--- BasicCard.js
+++ BasicCard.js
@@ -1,16 +1,17 @@
-module.exports = BasicCard;
+
 console.log("this loads")
 
 
 function BasicCard(front, back) {
     this.front = front;
     this.back = back;
-    this.printInfo = function() {
-    console.log(`${this.front} ${this.back}`);
-    }
+   
 }
 
 
-let firstPresident = new BasicCard ("Who was the first president of the United States", "George Washington");
+let bestPresident = new BasicCard ("Who was the best president ever", "Theodore Roosevelt");
 
-firstPresident.printInfo();
+module.exports = BasicCard;
+module.exports = bestPresident;
+
+
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,22 +1,39 @@
-module.exports = ClozeCard;
 
-var card = require("./BasicCard.js");
+var BasicCard = require("./BasicCard.js");
+var bestPresident = require("./BasicCard.js");
+
+
 
 function ClozeCard(text, cloze) {
-    this.text = text;
-    this.cloze = cloze;
-   
+    debugger;
+    text = process.argv.slice(2).join(" ");
+    cloze = bestPresident.back;
+    partial = bestPresident.front;
+    fullText = (`${bestPresident.front}? ${bestPresident.back}`);
+
+
+    if(text === cloze) {
+        console.log(`you got it right`);
+    }    else if (text == "") {
+        console.log(`YOU BLEW IT`);
+    }else if (text != cloze) {
+        console.log(`quess again`);
+    }
+  
+    
+    console.log(fullText);
+    console.log(partial);
+    console.log(cloze);
+
+    // cloze = BasicCard;
+    // console.log(firstPresident.front)
+    debugger;
+
+
+
 }
 
-ClozeCard.prototype.printInfo = function() {
-    console.log(`${this.text} ${this.cloze}`);
-}
 
-card.prototype.printInfo = function() {
-    console.log(`${this.text} ${this.cloze}`);
-}
+ClozeCard();
 
-let georgeWashington = new ClozeCard(car), "George Washington")
-
-georgeWashington.printInfo();
 
